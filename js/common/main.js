b26e8d77b9181421b77dbf7ea755767b8759036c
k343979 <yusuke040989@gmail.com>
2022-09-01 13:58:42 +0900

    contact validation

--- js/common/main.js
+++ js/common/main.js
@@ -5,6 +5,7 @@ $(function () {
     // ローディング画像を表示
     $('#loader-bg, #loader').height(h).css('display', 'block');
 });
+
 // 読み込み完了後に実行する
 $(window).on("load", function () {
     // ローディングをフェードアウト
@@ -16,4 +17,103 @@ $(window).on("load", function () {
 // ハンバーガーメニューのアニメーション
 $('.hamburger-btn').on('click', function () {
     $('#hamburgerContent').toggleClass('active');
+});
+
+// 問い合わせページ関連
+$(function () {
+    // 入力可能な文字数
+    var maxCount = 1000;
+
+    // 問い合わせ詳細の文字制限
+    $('#contact-detail').on('input', function () {
+        var count = $(this).val().length;
+        $('#detail-count').text(count + '/' + maxCount);
+        if (count > maxCount) {
+            $('#detail-count').css('color', '#ff0211');
+        }
+    });
+
+    // 送信ボタン押下時のバリデーション
+    $('#contactSubmit').on('click', function () {
+        // valueである文字列"lang=*"から"lang="を取り除きlangに格納
+        str = $(this).val();
+        // langのデフォルト
+        lang = '';
+        if (str.indexOf('lang=') != -1) {
+            lang = str.replace('lang=', '');
+        }
+
+        // id属性に"contact-"のプレフィクスが付く要素を取得
+        // 全ての入力項目が取得対象になる
+        $('[id^=contact-]').each(function () {
+            // required属性がついている、入力必須項目のバリデーション
+            if ($(this).attr('required') && $(this).val() == '') {
+                createAlert(this, lang, false);
+                $(this).css('border', '1px solid #ff0211');
+            }
+            // required属性がついているセレクトボックスのバリデーション
+            if ($(this).attr('required') && $(this).val() == '0') {
+                createAlert(this, lang, false);
+                $(this).css('border', '1px solid #ff0211');
+            }
+            // 入力可能な文字数のバリデーション
+            if ($(this).val() != '' && $(this).val().length > maxCount) {
+                createAlert(this, lang, true);
+                $(this).css('border', '1px solid #ff0211');
+            }
+        });
+    });
+
+    // アラートメッセージ生成
+    function createAlert(object, lang, countViolateFlag) {
+        // 入力項目のid属性を取得
+        id = $(object).attr('id');
+        // 入力項目のラベルを取得
+        label = $('label[for="' + id + '"]').text();
+        // タグの種別を取得
+        tagKind = $(object)[0].nodeName;
+        // タグの種別毎にエラーメッセージを分岐
+        switch (tagKind) {
+            case 'INPUT':
+                // デフォルト
+                $('.' + id).text('please input "' + label + '"');
+                // 日本語
+                if (lang == 'ja') {
+                    $('.' + id).text(label + 'を入力して下さい');
+                }
+                break;
+            case 'SELECT':
+                // デフォルト
+                $('.' + id).text('please select "' + label + '"');
+                // 日本語
+                if (lang == 'ja') {
+                    $('.' + id).text(label + 'を選択して下さい');
+                }
+                break;
+            case 'TEXTAREA':
+                // 文字数制限を超えていた場合
+                if (countViolateFlag) {
+                    // デフォルト
+                    $('.' + id).text('word count is over.');
+                    // 日本語
+                    if (lang == 'ja') {
+                        $('.' + id).text('文字数制限を超えています');
+                    }
+                } else {
+                    // デフォルト
+                    $('.' + id).text('please input "' + label + '"');
+                    // 日本語
+                    if (lang == 'ja') {
+                        $('.' + id).text(label + 'を入力して下さい');
+                    }
+                }
+                break;
+            default:
+                // 日本語
+                if (lang == 'ja') {
+                    $('.' + id).text(label + 'を確認して下さい');
+                }
+                break;
+        }
+    };
 });
